847739e0d556c6dda91206cf97791d1d04e2acd5
OhSeungHwan82 <108715101+OhSeungHwan82@users.noreply.github.com>
2024-02-16 17:20:51 +0900

    Update scheduled_task.py

--- app/service/scheduled_task.py
+++ app/service/scheduled_task.py
@@ -75,10 +75,9 @@ def scheduled_task():
                 qry = """
                         select count(*) cnt 
                           from ledger.ledger_database
-                         where info_request_pk = :info_request_pk
-                           and use_yb ='1'
+                         where info_request_pk = %s
                     """
-                pdb.execute(qry)
+                pdb.execute_bind(qry,(item[0],))
                 pfields = pdb.get_field_names()
                 pdatas = pdb.get_datas()
                 ld_cnt = pdatas[0][0]
@@ -132,3 +131,86 @@ def scheduled_task():
     pdb.close()
     db.close()
     
+def batchJob_scheduled_task():
+    print("배치관리 스케줄 시작")
+    jenkins_url = 'http://10.16.16.180:8080/'
+    jenkins_user = 'incar'
+    jenkins_token = '11ae9647a7cb4a55f1392d92ea4a0d04dd'
+
+    # 작업중인 job 에 대한 모니터링
+    pdb = PostgreLink()
+    qry="""
+        select 	pk 
+                , name 
+                , build_number
+        from 	job.job_request
+        where 	use_yb ='1'
+        and 	status = '2'
+    """
+    pdb.execute(qry)
+    pfields = pdb.get_field_names()
+    pdatas = pdb.get_datas()
+    for item in pdatas:
+        jr_pk = item[0]
+        job_name = item[1]
+        build_number = item[2]
+
+        api_url = f'{jenkins_url}/job/{job_name}/{build_number}/api/json'
+        headers = {
+            'Content-Type': 'application/xml',
+        }
+        response = requests.get(
+            api_url,
+            auth=(jenkins_user, jenkins_token),
+            headers=headers
+        )
+        if response.status_code == 200:
+            job_status = response.json()
+            if job_status['building']:
+                print(f'Job이 실행 중입니다. (빌드 번호: {job_status["number"]})')
+            else:
+                print(f'Job이 완료되었습니다. (빌드 번호: {job_status["number"]}, 결과: {job_status["result"]})')
+                try:
+                    qry="""
+                        update 	job.job_request
+                        set 	status = '3'
+                                , end_date = current_timestamp
+                        where 	pk = %s
+                    """
+                    pdb.execute_bind(qry , (jr_pk,))
+                except PsycopgError as e:
+                    pdb.close()
+                    raise HTTPException(status_code=500, detail=f"PostgreSQL Database error: {e}")
+        
+        api_url = f'{jenkins_url}/job/{job_name}/{build_number}/consoleText'
+        headers = {
+            'Content-Type': 'text/plain',
+        }
+
+        response = requests.get(
+            api_url,
+            auth=(jenkins_user, jenkins_token),
+            headers=headers
+        )
+
+        if response.status_code == 200:
+            console_output = response.text
+            print(f"console_output{console_output}")
+            try:
+                qry="""
+                    update 	job.job_request
+                    set 	log = %s
+                    where 	pk = %s
+                """
+                pdb.execute_bind(qry , (console_output, jr_pk,))
+            except PsycopgError as e:
+                pdb.close()
+                raise HTTPException(status_code=500, detail=f"PostgreSQL Database error: {e}")
+        else:
+            print(f'Console Output을 가져오는 중 오류가 발생했습니다. 응답 코드: {response.status_code}, 내용: {response.text}')
+    pdb.commit()
+    pdb.close()
+    print("배치관리 스케줄 종료")
+    # 상태가 진행중인 건에 대해서 스케줄러에서 5분마다 체크
+    # 상태가 완료 되면 모니터링 제외
+    # 작업요청 테이블에 상태 변경
