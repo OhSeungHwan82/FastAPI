241983c8b70233374f1bd47a78df8fd89c6ec7b8
OhSeungHwan82 <108715101+OhSeungHwan82@users.noreply.github.com>
2024-07-20 10:34:49 +0900

    Update batchJobModels.py

--- app/models/batchJobModels.py
+++ app/models/batchJobModels.py
@@ -3,22 +3,26 @@ from typing import List
 from fastapi import UploadFile
 from typing import Optional
 
+class KeyValueItem(BaseModel):
+    name: Optional[str] = None
+    value: Optional[str] = None
 class CreateJobTempleate(BaseModel):
     gubun:str
     name:str
     status:str
     description:Optional[str] = None
     executionscript:str
+    buildschedule:Optional[str] = None
+    params:Optional[List[KeyValueItem]] = None
+
+class ExecJobTempleate(BaseModel):
+    params:Optional[List[KeyValueItem]] = None
 
 class UpdateJobTempleate(BaseModel):
-    gubun:str
-    name:str
     status:str
     description:Optional[str] = None
-    executionscript:str
-    param1:Optional[str] = None
-    param2:Optional[str] = None
-    param3:Optional[str] = None
 
 class CreateJobRequest(BaseModel):
     jt_pk:int
+
+
