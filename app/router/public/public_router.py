9b2015be1f4145c37088231275fc2cc163aa7a9f
OhSeungHwan82 <108715101+OhSeungHwan82@users.noreply.github.com>
2024-07-20 10:38:26 +0900

    Update public_router.py

--- app/router/public/public_router.py
+++ app/router/public/public_router.py
@@ -28,23 +28,24 @@ def getlist(
 	list_field =[]
 	pdatas=[]
 	params=[]
-	qry = """
-			select 		cl_code
-					,	code_id
-					,	code_nm
-					,	use_yb
-			from		public.commcode
-			where 		cl_code = %s
-			and         use_yb in ('1')
-			order by	cl_code, order_no
-			"""
-	print("qry:",cl_code)
-	pdb.execute_bind(qry, (cl_code,))
-	pfields = pdb.get_field_names()
-	pdatas = pdb.get_datas()
-	# except PsycopgError as e:
-	# 	pdb.close()
-	# 	raise HTTPException(status_code=500, detail=f"PostgreSQL Database error: {e}")
+	try:
+		qry = """
+				select 		cl_code
+						,	code_id
+						,	code_nm
+						,	use_yb
+				from		public.commcode
+				where 		cl_code = %s
+				and         use_yb in ('1')
+				order by	cl_code, order_no
+				"""
+		print("qry:",cl_code)
+		pdb.execute_bind(qry, (cl_code,))
+		pfields = pdb.get_field_names()
+		pdatas = pdb.get_datas()
+	except PsycopgError as e:
+		pdb.close()
+		raise HTTPException(status_code=500, detail=f"PostgreSQL Database error: {e}")
 
 	results = {"list":[{field:value for field, value in zip(pfields, data)} for data in pdatas]}
 	pdb.close()
